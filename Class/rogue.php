ebb16408ad3157902ed3c2d263615b957a7e3e9e
Viperbi <nickels.florian@gmail.com>
2025-02-19 11:12:04 +0100

    translation finished and old files deleted

---
+++ Class/rogue.php
@@ -0,0 +1,7 @@
+<?php
+class Rogue extends AbstractCharacter {
+
+    public function display():void{
+        echo '<p>Je suis un Rogue';
+    }
+}
--- Class/voleur.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Voleur extends AbstractCharacter {
-
-    public function display():void{
-        echo '<p>Je suis un voleur';
-    }
-}
